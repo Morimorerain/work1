db45fb0f20e25a33c3dc32400ede311b6e33c385
Morimorerain <147136726+Morimorerain@users.noreply.github.com>
2023-10-28 18:50:34 +0800

    Add files via upload

--- my4/src/main.cpp
+++ my4/src/main.cpp
@@ -20,25 +20,35 @@ vex::motor RightMotor1(18);
 vex::motor RightMotor2(19);
 vex::motor tranMotor1(0);
 vex::motor tranMotor2(9,true);
-vex::motor shotmotor(1,true);
+vex::motor shotmotor1(1,true);
+vex::motor shotmotor2(8);
+vex::motor hangmotor1(10,true);
+vex::motor hangmotor2(11);
 vex::motor_group LeftMotorG(LeftMotor1,LeftMotor2);
 vex::motor_group RightMotorG(RightMotor1,RightMotor2);
 vex::motor_group tranMotorG(tranMotor1,tranMotor2);
+vex::motor_group shotmotorG(shotmotor1,shotmotor2);
+vex::motor_group hangmotorG(hangmotor1,hangmotor2);
+vex::vision vis(7);
 void tranballin();
 void tranballout();
-void down_toshot();
-void up_shot();
+void shotball();
+void hangit();
+void upit();
+int hangcount = 0;
 int vmax =180;
 int vmax2 = 50;
 int main() {
     void (*trin)();
     void (*trout)();
-    void (*down_st)();
-    void (*up_st)();
+    void (*stbl)();
+    void (*hag)();
+    void (*uit)();
     trin = tranballin;
     trout = tranballout;
-    down_st = down_toshot;
-    up_st = up_shot;
+    stbl = shotball;
+    hag = hangit;
+    uit = upit;
     while(1){
         vex::controller con1;
         int nf = con1.Axis3.position(pct);
@@ -51,9 +61,10 @@ int main() {
         RightMotorG.spin(fwd,v2,rpm);
         con1.ButtonR1.pressed(trout);        
         con1.ButtonL1.pressed(trin);
-        con1.ButtonDown.pressed(down_st);
-        con1.ButtonUp.pressed(up_st);
-        wait(10,msec);
+        con1.ButtonDown.pressed(stbl);
+        con1.ButtonRight.pressed(hag);
+        con1.ButtonLeft.pressed(uit);
+        this_thread::sleep_for(10);
     }
 }
 
@@ -73,11 +84,30 @@ void tranballout(){
         tranMotorG.stop();
     }
 }
-void down_toshot(){
-    shotmotor.resetPosition();
-    shotmotor.spinTo(90,deg,10,rpm,true);
+void shotball(){
+    shotmotorG.spin(fwd,10,rpm);
+    this_thread::sleep_for(4000);
+    while(1){
+        double m1 = shotmotor1.torque();
+        double m2 = shotmotor2.torque();
+        double m = m1 + m2;
+        if(m>0.78){
+            shotmotorG.stop(hold);
+            break;
+        }
+        else{
+            this_thread::sleep_for(10);
+            continue;
+        }
+    }
 }
-void up_shot(){
-    shotmotor.resetPosition();
-    shotmotor.spinTo(270,deg,10,rpm,true);
-}
+void hangit()
+{hangmotorG.spin(fwd,30,rpm);
+this_thread::sleep_for(7500);
+hangmotorG.spin(fwd,0,rpm);
+hangmotorG.stop(hold);}
+void upit()
+{hangmotorG.spin(fwd,-30,rpm);
+this_thread::sleep_for(7500);
+hangmotorG.spin(fwd,0,rpm);
+hangmotorG.stop(hold);}
